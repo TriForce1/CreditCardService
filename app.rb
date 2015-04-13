bf2df8defd67ef8fbb238193781b2094ee69eebc
tnfontenelle <tnfontenelle@hotmail.com>
2015-04-13 21:42:01 +0800

    Added Credit card validation route

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require_relative './lib/credit_card.rb'
 
+# Credit Card Web Service
 class CreditCardAPI < Sinatra::Base
 
+  get '/api/v1/credit_card/validate' do
+    c = CreditCard(params[:text],nil,nil,nil)
+    {"Card" => params[text], "validated" => c.validate_checksum}.to_json
+  end
 
 
 
@@ -14,5 +20,6 @@ class CreditCardAPI < Sinatra::Base
 
 
 
-  
+
+
 end
