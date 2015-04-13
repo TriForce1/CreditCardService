6e1c83d5dd278a9bb6e382b08a469664625a1f52
chenlizhan <chung1350@hotmail.com>
2015-04-13 22:25:21 +0800

    Completed root route

--- app.rb
+++ app.rb
@@ -4,22 +4,13 @@ require_relative './lib/credit_card.rb'
 # Credit Card Web Service
 class CreditCardAPI < Sinatra::Base
 
+  get '/' do
+    'The CreditCardAPI is up and running!'
+  end
+
   get '/api/v1/credit_card/validate' do
     c = CreditCard.new(params[:card_number],nil,nil,nil)
     {"Card" => params[:card_number], "validated" => c.validate_checksum}.to_json
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 end
