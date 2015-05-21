bedbc6d7978d1992cf81b7b90ea693ad4b42c30f
Clive <CliveImpisa>
2015-05-21 11:57:40 +0800

    Fixed some errors

--- helpers/creditcard_helper.rb
+++ helpers/creditcard_helper.rb
@@ -91,6 +91,7 @@ module CreditCardHelper
   end
 
   def create_user_with_encrypted_token(token_enc)
+    puts "token #{token_enc}"
     token = decrypt_message(token_enc)
     payload = (JWT.decode token, ENV['MSG_KEY']).first
     puts "(tTHE PAYLOADS: (#{payload}))"
